e520cb6b5b5d4d99aa82bacc4f879d9c552e2041
RicoBasyar <RicoBasyar@users.noreply.github.com>
2020-07-16 06:00:26 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# ricobasyar
+# ricobasyar
---
+++ modal.js
@@ -0,0 +1,40 @@
+var modal = document.getElementById('modal');
+var modalbtn = document.getElementById('btnOpen');
+
+const nav = document.querySelector('nav');
+const logo = document.querySelector('.logo');
+const input = document.querySelector('.inputt');
+const ul = document.querySelector('nav ul');
+
+modalbtn.addEventListener('click', openmodal);
+window.addEventListener('click', closeoutside);
+
+function openmodal(){
+    modal.style.display = 'flex';
+}
+
+function closeoutside(e){
+    if(e.target == modal){
+    modal.style.display = 'none';
+    }
+}
+
+window.onscroll = function(){
+    var top = window.scrollY;
+    if(top >= 50){
+        nav.classList.add('active');
+        logo.classList.add('active');
+        ul.classList.add('active');
+    }else{
+        nav.classList.remove('active');
+        logo.classList.remove('active');
+        ul.classList.remove('active');
+    }
+}
+
+function scrollwin(){
+    window.scrollTo(0,600);
+}
+
+
+
