5552b4a2fd1259f2e2ea74adac795df9edc97c2b
RicoBasyar <RicoBasyar@users.noreply.github.com>
2020-07-16 14:40:38 +0700

    forth commit

--- modal.js
+++ modal.js
@@ -1,12 +1,14 @@
-var modal = document.getElementById('modal');
-var modalbtn = document.getElementById('btnOpen');
-var body = document.getElementsByTagName('body')
+const modal = document.getElementById('modal');
+const modalbtn = document.getElementById('btnOpen');
+const body = document.getElementsByTagName('body')
 
 const nav = document.querySelector('nav');
 const logo = document.querySelector('.logo');
 const input = document.querySelector('.inputt');
 const ul = document.querySelector('nav ul');
 const tatas = document.querySelector('.tombolatas');
+const filepre = document.getElementById("inputfile");
+const previewimg = document.querySelector(".image-preview__image");
 
 modalbtn.addEventListener('click', openmodal);
 window.addEventListener('click', closeoutside);
@@ -20,6 +22,25 @@ function closeoutside(e){
     modal.style.display = 'none';
     }
 }
+
+filepre.addEventListener("change", function(){
+    const file = this.files[0];
+
+    if (file) {
+        const reader = new FileReader();
+
+        previewimg.style.display = "block";
+
+        reader.addEventListener("load", function() {
+            previewimg.setAttribute("src", this.result);
+        });
+
+        reader.readAsDataURL(file);
+    }else{
+        previewimg.style.display = null;
+        previewimg.setAttribute("src", "");
+    }
+});
 
 window.onscroll = function(){
     var top = window.scrollY;
