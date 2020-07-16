f5a9846fdee0f7343bcc2c10eb269745f25349b9
RicoBasyar <RicoBasyar@users.noreply.github.com>
2020-07-16 14:49:53 +0700

    forth commit

--- modal.js
+++ modal.js
@@ -7,8 +7,8 @@ const logo = document.querySelector('.logo');
 const input = document.querySelector('.inputt');
 const ul = document.querySelector('nav ul');
 const tatas = document.querySelector('.tombolatas');
-const filepre = document.getElementById("inputfile");
-const previewimg = document.querySelector(".image-preview__image");
+const filepre = document.getElementById('inputfile');
+const previewimg = document.querySelector('.image-preview__image');
 
 modalbtn.addEventListener('click', openmodal);
 window.addEventListener('click', closeoutside);
@@ -22,25 +22,6 @@ function closeoutside(e){
     modal.style.display = 'none';
     }
 }
-
-filepre.addEventListener("change", function(){
-    const file = this.files[0];
-
-    if (file) {
-        const reader = new FileReader();
-
-        previewimg.style.display = "block";
-
-        reader.addEventListener("load", function() {
-            previewimg.setAttribute("src", this.result);
-        });
-
-        reader.readAsDataURL(file);
-    }else{
-        previewimg.style.display = null;
-        previewimg.setAttribute("src", "");
-    }
-});
 
 window.onscroll = function(){
     var top = window.scrollY;
@@ -64,6 +45,26 @@ function scrollwin(){
 function scrollup(){
     window.scrollTo(0,0);
 }
+
+filepre.addEventListener('change', function() {
+    const file = this.files[0];
+
+    if (file) {
+        const reader = new FileReader();
+
+        previewimg.style.display = "block";
+
+        reader.addEventListener("load", function() {
+            previewimg.setAttribute("src", this.result);
+        });
+
+        reader.readAsDataURL(file);
+    }else{
+        previewimg.style.display = null;
+        previewimg.setAttribute("src", "");
+    }
+});
+
 
 
 
